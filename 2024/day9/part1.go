1820df31b9212b013f5c0f64c207e14de5d06b10
Marty Mammel <mmammel12@gmail.com>
2024-12-11 09:28:07 -0600

    day 9

--- 2024/day9/part1.go
+++ 2024/day9/part1.go
@@ -1,7 +1,82 @@
 // Package day9 -
 package day9
 
+import (
+	"strconv"
+	"strings"
+)
+
+type mem struct {
+	idx    int
+	amount int
+}
+
 // Part1 -
 func Part1(lines []string) (int, error) {
-	return 0, nil
+	line := lines[0]
+	nums := strings.Split(line, "")
+
+	mems := make([]mem, 0)
+
+	idx := 0
+	totalLen := 0
+	for i, numStr := range nums {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return 0, err
+		}
+
+		if i%2 == 0 {
+			// block
+			mems = append(mems, mem{idx, num})
+			idx++
+			totalLen += num
+		} else {
+			// empty
+			mems = append(mems, mem{-1, num})
+		}
+	}
+
+	memory := make([]int, totalLen)
+	j := 0
+	k := len(mems) - 1
+	if mems[k].idx == -1 {
+		k--
+	}
+	for i := 0; i < len(mems); i++ {
+		if j >= totalLen {
+			break
+		}
+		if mems[i].idx != -1 {
+			for mems[i].amount > 0 {
+				memory[j] = mems[i].idx
+				mems[i].amount--
+				j++
+				if j >= totalLen {
+					break
+				}
+			}
+		} else {
+			for mems[i].amount > 0 {
+				memory[j] = mems[k].idx
+				mems[k].amount--
+				mems[i].amount--
+				j++
+
+				if mems[k].amount == 0 {
+					k -= 2
+				}
+				if j >= totalLen {
+					break
+				}
+			}
+		}
+	}
+
+	ans := 0
+	for i, val := range memory {
+		ans += i * val
+	}
+
+	return ans, nil
 }
--- 2024/day9/part2.go
+++ 2024/day9/part2.go
@@ -1,7 +1,85 @@
 // Package day9 -
 package day9
 
+import (
+	"strconv"
+	"strings"
+)
+
+type mem2 struct {
+	idx    int
+	amount int
+	used   bool
+}
+
 // Part2 -
 func Part2(lines []string) (int, error) {
-	return 0, nil
+	line := lines[0]
+	nums := strings.Split(line, "")
+
+	mems := make([]mem2, 0)
+
+	idx := 0
+	totalLen := 0
+	for i, numStr := range nums {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return 0, err
+		}
+
+		totalLen += num
+		if i%2 == 0 {
+			// block
+			mems = append(mems, mem2{idx, num, false})
+			idx++
+		} else {
+			// empty
+			mems = append(mems, mem2{-1, num, false})
+		}
+	}
+
+	memory := make([]mem2, 0)
+	for i, loc := range mems {
+		if loc.idx >= 0 {
+			memory = append(memory, loc)
+		} else {
+			for j := len(mems) - 1; j > i; j-- {
+				if mems[j].idx == -1 || mems[j].used == true {
+					continue
+				}
+
+				if mems[j].amount <= loc.amount {
+					memory = append(memory, mems[j])
+					mems[j].used = true
+					loc.amount -= mems[j].amount
+				}
+			}
+
+			if loc.amount > 0 {
+				memory = append(memory, mem2{-1, loc.amount, true})
+			}
+		}
+	}
+
+	memoryInts := make([]int, 0)
+	for _, loc := range memory {
+		if loc.idx == -1 || loc.used {
+			for i := 0; i < loc.amount; i++ {
+				memoryInts = append(memoryInts, -1)
+			}
+		} else {
+			for i := 0; i < loc.amount; i++ {
+				memoryInts = append(memoryInts, loc.idx)
+			}
+		}
+	}
+
+	ans := 0
+	for i, val := range memoryInts {
+		if val != -1 {
+			ans += i * val
+		}
+	}
+
+	return ans, nil
 }
